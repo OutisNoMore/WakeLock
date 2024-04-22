66e62f63db010a99b14c73184a6c3ef20463f056
OutisNoMore <OutisNoMore@protonmail.com>
2024-04-21 18:41:58 -0700

    - Move wake lock code into onCreate function. - Untested launch app on boot up

--- app/src/main/java/com/example/wakelock/MainActivity.kt
+++ app/src/main/java/com/example/wakelock/MainActivity.kt
@@ -5,18 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PowerManager
 
+import android.util.Log
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    // Create button to release/acquire
-    val wakeLock: PowerManager.WakeLock =
-        (getSystemService(Context.POWER_SERVICE) as PowerManager).run {
-            newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MyApp::MyWakelockTag").apply {
-                acquire()
+        Log.d("test", "App run")
+
+        // Acquire CPU wake lock
+        val wakeLock: PowerManager.WakeLock =
+            (getSystemService(Context.POWER_SERVICE) as PowerManager).run {
+                newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "WakeLock::MyWakelockTag").apply {
+                    acquire() // Do not release
+                }
             }
-        }
-
+    }
 }
